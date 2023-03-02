0229e724be0c00ad0dc15c4b94304a5b5272acd0
Eri <11618062+delaluzEri@users.noreply.github.com>
2023-03-02 15:09:54 -0600

    update c_funcionarios campo c_funcionario_anterior

---
+++ update c_funcionarios campo c_funcionario_anterior.sql
@@ -0,0 +1,63 @@
+/*
+ * ACTUALIZACIÓN DE C_FUNCIONARIOS
+ * Se actualiza campo de c_funcionario_anterior, con base en:
+ * Análisis con Nancy y con base a las claves que tenían en tabla de usuarios producción
+ */
+
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '215' WHERE (`id` = '1130');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '274' WHERE (`id` = '1174');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '955' WHERE (`id` = '1000');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '291' WHERE (`id` = '1005');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '840' WHERE (`id` = '1138');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '226' WHERE (`id` = '1168');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '350' WHERE (`id` = '1180');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '831' WHERE (`id` = '1166');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '137' WHERE (`id` = '967');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '223' WHERE (`id` = '1082');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '152' WHERE (`id` = '1148');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '181' WHERE (`id` = '1018');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '234' WHERE (`id` = '1221');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '354' WHERE (`id` = '971');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '608' WHERE (`id` = '1136');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '746' WHERE (`id` = '1059');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '254' WHERE (`id` = '1172');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '744' WHERE (`id` = '1061');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '318' WHERE (`id` = '1135');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '8' WHERE (`id` = '1145');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '616' WHERE (`id` = '1143');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '170' WHERE (`id` = '986');
+UPDATE `c_funcionario` SET `c_funcionario_anterior` = '324' WHERE (`id` = '1156');
+
+
+/*Update con datos de usuario-funcionario*/
+update c_funcionario set c_funcionario_anterior = '959' where id = '966';
+update c_funcionario set c_funcionario_anterior = '136' where id = '973';
+update c_funcionario set c_funcionario_anterior = '738' where id = '978';
+update c_funcionario set c_funcionario_anterior = '249' where id = '987';
+update c_funcionario set c_funcionario_anterior = '90' where id = '993';
+update c_funcionario set c_funcionario_anterior = '178' where id = '1013';
+update c_funcionario set c_funcionario_anterior = '193' where id = '1030';
+update c_funcionario set c_funcionario_anterior = '212' where id = '1033';
+update c_funcionario set c_funcionario_anterior = '204' where id = '1043';
+update c_funcionario set c_funcionario_anterior = '717' where id = '1053';
+update c_funcionario set c_funcionario_anterior = '926' where id = '1054';
+update c_funcionario set c_funcionario_anterior = '151' where id = '1054';
+update c_funcionario set c_funcionario_anterior = '150' where id = '1055';
+update c_funcionario set c_funcionario_anterior = '138' where id = '1056';
+update c_funcionario set c_funcionario_anterior = '796' where id = '1062';
+update c_funcionario set c_funcionario_anterior = '803' where id = '1078';
+update c_funcionario set c_funcionario_anterior = '238' where id = '1079';
+update c_funcionario set c_funcionario_anterior = '218' where id = '1084';
+update c_funcionario set c_funcionario_anterior = '357' where id = '1087';
+update c_funcionario set c_funcionario_anterior = '822' where id = '1090';
+update c_funcionario set c_funcionario_anterior = '737' where id = '1107';
+update c_funcionario set c_funcionario_anterior = '265' where id = '1127';
+update c_funcionario set c_funcionario_anterior = '315' where id = '1134';
+update c_funcionario set c_funcionario_anterior = '262' where id = '1139';
+update c_funcionario set c_funcionario_anterior = '230' where id = '1152';
+update c_funcionario set c_funcionario_anterior = '322' where id = '1153';
+update c_funcionario set c_funcionario_anterior = '834' where id = '1154';
+update c_funcionario set c_funcionario_anterior = '300' where id = '1160';
+update c_funcionario set c_funcionario_anterior = '159' where id = '1167';
+update c_funcionario set c_funcionario_anterior = '294' where id = '1198';
+update c_funcionario set c_funcionario_anterior = '198' where id = '1223';
